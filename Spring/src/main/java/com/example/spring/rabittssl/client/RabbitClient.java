63830bbcc6c2904d1154449655b5b325cf1ec500
robert.lee <robert.lee@iisigroup.com>
2025-05-09 11:29:00 +0800

    add pom.xml

--- Spring/src/main/java/com/example/spring/rabittssl/client/RabbitClient.java
+++ Spring/src/main/java/com/example/spring/rabittssl/client/RabbitClient.java
@@ -22,7 +22,6 @@ import org.springframework.amqp.rabbit.connection.Connection;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
